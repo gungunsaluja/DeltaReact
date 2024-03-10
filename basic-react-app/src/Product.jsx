dfb943783cb8482bbb918e6734dbf3a6209e7dfe
gungunsaluja <gungunsaluja16@gmail.com>
2024-03-10 22:51:31 +0530

    commit

--- basic-react-app/src/Product.jsx
+++ basic-react-app/src/Product.jsx
@@ -1,16 +1,36 @@
 import "./Product.css";
 
-function Product({title,price,features,features2}){
+function Product({ title, price ,features }){
+    let isDiscount = price>30000;
     
-     
-    return(
-        <div className = "Product">
-            <h3>{title}</h3>
-            <h5>price:{price}</h5>
-            <p>{features.a}</p>
-            {/* <p>{features2.b}</p> */}
-             
-        </div>
-    );
+    let styles = {backgroundColor: isDiscount?"pink":""};
+        return(
+            <div className = "Product"style ={styles} >
+                <h3  >{title}</h3>
+                <h5 >price : {price}</h5>
+                {/* <p>Discount of 5%</p> */}
+                {/* <p>{isDiscount}</p> */}
+                {isDiscount? <p>"Discount of 5%"</p> :<a href = "/">Get Discount</a>}
+                {/* <p>{list}</p> */}
+                {/* <p>{features2.b}</p> */}
+                 
+            </div>
+        );
+
+   
+    // else{
+    //     return(
+    //         <div className = "Product">
+    //             <h3>{title}</h3>
+    //             <h5>price : {price}</h5>
+    //             {/* <p>Discount of 5%</p> */}
+    //             {/* <p>{list}</p> */}
+    //             {/* <p>{features2.b}</p> */}
+                 
+    //         </div>
+    //     );
+    // }
+    //  const list = features.map((feature) => <li>{feature}</li>);
+   
 }
 export default Product;
--- basic-react-app/src/ProductTab.jsx
+++ basic-react-app/src/ProductTab.jsx
@@ -1,12 +1,15 @@
 import Product from "./Product.jsx";
 function ProductTab(){
-    let options = ["hi-tech","durable","fast"];
+    // let options = [<li>hi-tech</li>,<li>durable</li>,<li>fast</li>]; This could be a way to represent the list items
+    let options = ["hi-tech" , "durable" , "fast"];
     // let options2 = {a:"hi-tech",b:"durable",c:"fast"};
     return (
         <>
-        <Product title = "phone" price = {3000} features ={{a:"hi-tech"}} />
-         {/* <Product title = "laptop" price = {45000} />
-        <Product title = "Pen" price = {87000}/> */}
+        <Product title = "phone" price = {3000} />
+         <Product title = "laptop" price = {45000} />
+
+        <Product title = "Pen" price = {87000}/>
+
         </>
         );
 }
